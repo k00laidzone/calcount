aa85649cf5a40a38e954c3e3f1e859da78dd7145
Shawn Pick <shanwpick@gmail.com>
2014-03-17 13:17:31 -0600

    added MySql

--- CalCount/Migrations/Configuration.cs
+++ CalCount/Migrations/Configuration.cs
@@ -5,11 +5,19 @@ namespace CalCount.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
+    using MySql.Data.MySqlClient;
+    using MySql.Data;
+    using MySql.Data.Entity;
+
+
+
     internal sealed class Configuration : DbMigrationsConfiguration<CalCount.Models.CaloriesDataContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+
+            SetSqlGenerator("MySql.Data.MySqlClient", new MySql.Data.Entity.MySqlMigrationSqlGenerator());
         }
 
         protected override void Seed(CalCount.Models.CaloriesDataContext context)
